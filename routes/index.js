9eaa9cec11dbc4d26223f65124ccab05d8e4af12
DanielSBBD <daniels@bbd.co.za>
2023-07-22 11:46:48 +0200

    Added login page

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get("/", function (req, res, next) {
   res.render("index", { title: "Which train are you?" });
 });
 
+router.get("/login", function (req, res, next) {
+  res.render("login", { title: "Please Log In" });
+});
+
 module.exports = router;
