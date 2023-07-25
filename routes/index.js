2b718a788d0b52d9553ee4d901096d121ce38b63
Line Dorfling <line@bbd.co.za>
2023-07-25 14:08:11 +0200

    basic quiz page with mock data

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,38 @@ router.get("/", function (req, res, next) {
 });
 
 router.get("/login", function (req, res, next) {
-  res.render("login", { title: "Please Log In" });
+  res.render("login", { title: "Login" });
+});
+
+// TODO: Add in the api url
+const API_URL = "";
+
+// This is temp:
+const questions = [
+  {
+    question: "I always have kind things to say to other people.",
+    id: 1,
+  },
+  {
+    question: "I am a good listener.",
+    id: 2,
+  },
+  {
+    question: "I often feel down or sad.",
+    id: 3,
+  },
+];
+
+router.get("/quiz", function (req, res, next) {
+  // try {
+  // const response = await axios.get(API_URL);
+  // const questions = response.data;
+  // res.render("quiz", { questions });
+  // } catch (error) {
+  // console.error("Error fetching questions:", error.message);
+  // res.render("error");
+  // }
+  res.render("quiz", { title: "Which train are you?", questions: questions });
 });
 
 module.exports = router;
