98e30eac1b1b27ab1a44db8508850347eaffc58c
DanielSBBD <daniels@bbd.co.za>
2023-07-27 16:12:10 +0200

    Deleted score field

--- backend/WhichTrainAreYouAPI/Models/AppUser.cs
+++ backend/WhichTrainAreYouAPI/Models/AppUser.cs
@@ -1,13 +1,12 @@
 ﻿namespace WhichTrainAreYouAPI.Models
 {
-    public class AppUser
-    {
-        public int UserId { get; set; }
-        public string Username { get; set; }
-        public string PasswordHash { get; set; }
-        public string Salt { get; set; }
-        public int Score { get; set; }
-        public int TrainId { get; set; }
-    }
+  public class AppUser
+  {
+    public int UserId { get; set; }
+    public string Username { get; set; }
+    public string PasswordHash { get; set; }
+    public string Salt { get; set; }
+    public int TrainId { get; set; }
+  }
 
 }
